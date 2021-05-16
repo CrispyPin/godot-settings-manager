858b6502e33c93f22120be20de1e0cd83544d336
CrispyPin <crispin@tasa.se>
2021-05-16 13:11:04 +0200

    fix typo

--- addons/settings-manager/settings_manager.gd
+++ addons/settings-manager/settings_manager.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-    add_autoload_singleton("Settings", "res://addons/setings-manager/settings.gd")
+    add_autoload_singleton("Settings", "res://addons/settings-manager/settings.gd")
 
 
 func _exit_tree() -> void:
